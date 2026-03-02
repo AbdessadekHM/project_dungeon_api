7db0dbfbb066eb1c15a2b812b4795a78b13cd4d5
Abdessadek <abdessadekhm@gmail.com>
2026-03-02 12:19:07 +0000

    [ADD] Add user register & loging endpoints

--- account/views.py
+++ account/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer
 
 # Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+    
+
