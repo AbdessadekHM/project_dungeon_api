d141c933c03293e6a26223d4d082c8331a4f97d0
Abdessadek <abdessadekhm@gmail.com>
2026-03-03 14:16:27 +0000

    [ADD] Declare patch and delete methods for management's model

--- apps/management/urls.py
+++ apps/management/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import ProjectViewSet, TaskViewSet, TeamViewSet, RepositoryViewSet, EventViewSet
 
 urlpatterns = [
-    path("projects/", ProjectViewSet.as_view(actions={"get": "list", "post": "create"}), name="projects"),
-    path("tasks/", TaskViewSet.as_view(actions={"get": "list", "post": "create"}), name="tasks"),
+    path("projects/", ProjectViewSet.as_view(actions={"get": "list", "post": "create", "patch": "partial_update", "delete": "destroy"}), name="projects"),
+    path("tasks/", TaskViewSet.as_view(actions={"get": "list", "post": "create", "patch": "partial_update", "delete": "destroy"}), name="tasks"),
+    path("teams/<int:pk>/", TeamViewSet.as_view(actions={"get": "retrieve", "patch": "partial_update", "delete": "destroy"}), name="teams"),
     path("teams/", TeamViewSet.as_view(actions={"get": "list", "post": "create"}), name="teams"),
-    path("repositories/", RepositoryViewSet.as_view(actions={"get": "list", "post": "create"}), name="repositories"),
-    path("events/", EventViewSet.as_view(actions={"get": "list", "post": "create"}), name="events"),
-    
+    path("repositories/", RepositoryViewSet.as_view(actions={"get": "list", "post": "create", "patch": "partial_update", "delete": "destroy"}), name="repositories"),
+    path("events/", EventViewSet.as_view(actions={"get": "list", "post": "create", "patch": "partial_update", "delete": "destroy"}), name="events"),
 ]
