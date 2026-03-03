d73e8f001f7d3c2e0fb0bea51d54879e3d50fd06
Abdessadek <abdessadekhm@gmail.com>
2026-03-03 14:05:31 +0000

    [UPD] Register models in the admin site

--- apps/management/admin.py
+++ apps/management/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Project, Task, Team, Repository, Event
 # Register your models here.
+
+admin.site.register(Project)
+admin.site.register(Task)
+admin.site.register(Team)
+admin.site.register(Repository)
+admin.site.register(Event)
