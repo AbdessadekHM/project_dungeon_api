481102f5c4ca7dc3009c2cac3a1c8c3e99fd70cf
Abdessadek <abdessadekhm@gmail.com>
2026-03-03 11:59:50 +0000

    [ADD] Automate owner affectation for a created project

--- apps/management/views.py
+++ apps/management/views.py
@@ -10,6 +10,9 @@ class BaseProjectViewSet(viewsets.ModelViewSet):
 class ProjectViewSet(BaseProjectViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class TaskViewSet(BaseProjectViewSet):
